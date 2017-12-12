847a0298a288d0fdf7b9a5be2d0c56eb423a8e2f
mca <michel.caluwaerts@student.unamur.be>
2017-12-12 23:07:39 +0100

    1ere version 100% fonctionnelle

--- squelette.pl
+++ squelette.pl
@@ -1,70 +1,197 @@
-% Student exercise profile
-:- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
-:- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
-:- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
 :- style_check(- singleton).
-:- use_module(library(clpfd)). % librairie de programmation logique
-                               % par contrainte à domaine fini
+:- use_module(library(clpfd)).
+:- use_module(library(statistics)).
 
-testNoms([  anne, michel, manon, alice, juliette, lucie, charlotte, olivia, margaux,
-            jules, hugo, tom, louis, paul, jean, antoine  ]).
+% ---------------------------------------------------------------------- %
+%                                                                        %
+%                        PREDICAT PRINCIPAL                              %
+%                                                                        %
+% ---------------------------------------------------------------------- %
+noms([  manon, alice, juliette, lucie, charlotte,
+        olivia, margaux,jules, hugo, tom, louis, paul, jean, antoine  ]).
 
-go(Lassoc) :-
-    testNoms(L),
-    length(L,Nb),
-    produire_liste(L,Lassoc),
-    term_variables(Lassoc,Labels),
-    all_different(Labels),
-    mme_mr(Lassoc,Nb),
-    alternate(Lassoc),
-    label(Labels),
-    write(Labels).
 
-mme_mr([Mme|T],Nb):-
-    getvar(Mme,VarMme),
-    1 #=  VarMme,
-    nth0(0,T,Mr),
-    getvar(Mr,VarMr),
+go :- noms(Lconvives),
+      diner(Lconvives).
+
+diner(Lconvives) :-
+
+    length([anne,michel|Lconvives],Nb),
+    produire_lassoc([anne,michel|Lconvives],Lassoc, Nb),
+    produire_ldonnees([anne,michel|Lconvives],Data),
+    alternance_homme_femme(Lassoc,Nb),
+    pas_epoux_cote_a_cote(Lassoc,Data,Nb),
+    meme_hobby(Lassoc, Nb, Data),
+    pas_incompatibilite(Data, Lassoc),
+    label_places(Lassoc,Nb),
+    sort(2,@<,Lassoc,Lassoc_trie),
+    writeln(Lassoc_trie),
+    impression_table(Lassoc_trie).
+
+
+% ---------------------------------------------------------------------- %
+%                          LISTE ASSOCIATIVE                             %
+% ---------------------------------------------------------------------- %
+produire_lassoc( [], _, [] ).
+produire_lassoc(List,Lassoc, Nb):-
+    produire_persvar(List, Lassoc),
+    term_variables(Lassoc,ListVar),
+    ListVar ins 1..Nb,
+    mme_mr(ListVar,Nb).
+
+/**************************************************************************/
+% produire_persvar(Liste_de_convives, Liste_de_pers_Var)
+
+produire_persvar([],[]).
+produire_persvar([H|T],[pers_var(H,Var)|Rest]):-
+    produire_persvar(T,Rest).
+
+/**************************************************************************/
+% mme_mr( pers_var(Madame), pers_var(Monsieur)
+%
+
+mme_mr([Mme,Mr|_],Nb):-
+    Mme in 1..1,
     Mid is Nb/2,
-    Mid #= VarMr.
-
-alternate([]).
-alternate([H]).
-alternate([H|T]):-
-    get_suivant(T,Suivant),!,
-    getname(H,N1),
-    getname(Suivant,NameSuiv),
-    genre_compatible(N1,NameSuiv),
-    alternate(T).
-
-get_suivant([],[]) .
-get_suivant([H|T],H) .
-
-
-getname(PersVar,Name):-
-    Persvar=(Name,_).
-
-genre_compatible(Name1, Name2) :-
-    sexe(Name1, Sexe1),!, sexe(Name2,Sexe2),not(Sexe1=Sexe2).
-
+    Mr in Mid..Mid.
 
 getvar(PersVar,Var):-
     PersVar=pers_var(_,Var).
 
-produire_liste([],[]).
-produire_liste([H|T],[pers_var(H,Var)|Rest]):-
-    Var in 1..16,
-    produire_liste(T,Rest).
+getPers_var(Pers, Var, List):-
+    member(pers_var(Pers, Var), List).
 
-getlist([], []).
-getlist([H|T], [Var|Rest]):-
-    X=pers_var(N,Var),
-    getlist(T,Rest).
 
-makenew([], []).
-makenew([H|T], [pers_var(N,H)|Rest]):-
-        makenew(T,Rest).
+produire_ldonnees([], []).
+produire_ldonnees([H|T], [p(H,S,Part,Hob,Incomp)|Rest]):-
+    sexe(H,S),
+    partenaire(H,Part),
+    hobby(H,Hob),
+    incompatibilites(H,Incomp),
+    produire_ldonnees(T, Rest).
 
+incompatibilites(X,[E]):-
+    incompatible(X,E).
+incompatibilites(X,[]):-
+   not(incompatible(X,_)).
+partenaire(X,Partenaire):-
+    epoux(X,Partenaire),!.
+partenaire(X,Partenaire):-
+    epoux(Partenaire,X).
+
+% ---------------------------------------------------------------------- %
+%                                                                        %
+%                               CONTRAINTES                              %
+%                                                                        %
+% ---------------------------------------------------------------------- %
+
+% Alternance homme-femme
+% ----------------------
+
+alternance_homme_femme( [],_ ).
+alternance_homme_femme( [pers_var(P,V)|Rest],Nb ) :-
+    (   sexe(P,m) *-> true,
+         V mod 2 #= 0
+    ;   true,   V mod 2 #= 1
+    ),
+    alternance_homme_femme( Rest,Nb ).
+
+
+% Pas epoux cote a cote
+% ---------------------
+pas_epoux_cote_a_cote(_,[],_ ).
+pas_epoux_cote_a_cote(Lassoc, [p(Name1,_,Epoux_1,_,_)|T], Nb ):-
+    getPers_var(Epoux_1, VarEpoux1, Lassoc),
+    getPers_var(Name1,VarName1, Lassoc),
+    abs(VarEpoux1 mod 16 - VarName1 mod 16)#>1,
+    pas_epoux_cote_a_cote(Lassoc, T, Nb).
+
+
+% Meme hobby
+% ----------
+
+meme_hobby( [], _, _ ).
+meme_hobby( [pers_var(Name1, Var1)|T], Nb, Data ) :-
+     member(p(Name1,_,_,Hobby_1,_), Data),
+     l_incompatibles(Hobby_1, Data, Incompatibles),
+     eliminer_hobby_incomp(Incompatibles, Var1, Lassoc),
+     meme_hobby(T, Nb, Data).
+
+eliminer_hobby_incomp([], _, _).
+eliminer_hobby_incomp([H|T], VarName, Lassoc):-
+    getPers_var(H, Var2, Lassoc),
+    abs(VarName mod 16 - Var2 mod 16)#>1,
+    eliminer_hobby_incomp(T, VarName, Lassoc).
+
+
+
+l_incompatibles(Hobby_1, [], []).
+l_incompatibles( Hobby_1, [p(_,_,_,Lhobbies,_)|T], Rest):-
+    intersection(Hobby_1, Lhobbies, X),
+    length(X,N), N#>0,
+    l_incompatibles(Hobby_1, T, Rest).
+
+l_incompatibles( Hobby_1, [p(Name,_,_,Lhobbies,_)|T], [Name|Rest]):-
+    intersection(Hobby_1, Lhobbies, []),
+    l_incompatibles(Hobby_1, T, Rest).
+
+
+
+
+% Pas d incompatibilite
+% ---------------------
+
+pas_incompatibilite([], Lassoc ).
+
+pas_incompatibilite([p(Name1,_,_,_,[])|T], Lassoc ):-
+    pas_incompatibilite(T,Lassoc).
+
+pas_incompatibilite([p(Name1,_,_,_,Incompatibles)|T], Lassoc ):-
+    length(Incompatibles,L), L#>0,
+    getPers_var(Name1, Var1, Lassoc),
+    eliminer_ennemis(Incompatibles, Var1, Lassoc),
+    pas_incompatibilite(T, Lassoc).
+
+eliminer_ennemis([], _, Lassoc).
+eliminer_ennemis([E|Ennemis], Var, Lassoc):-
+    getPers_var(E, VarE, Lassoc),
+    abs(Var mod 16 - VarE mod 16 )#>1,
+    eliminer_ennemis(Ennemis, Var, Lassoc).
+
+
+
+% ---------------------------------------------------------------------- %
+%                                                                        %
+%                                LABELING                                %
+%                                                                        %
+% ---------------------------------------------------------------------- %
+
+label_places(Lassoc,Nb):-
+    get_variables(Lassoc,Labels),
+    all_different(Labels),
+    label(Labels).
+
+get_variables([], []).
+get_variables([H|T], [VarH|Rest]):-
+    getvar(H,VarH),
+    get_variables(T, Rest).
+
+% ---------------------------------------------------------------------- %
+%                                                                        %
+%                                IMPRESSION                              %
+%                                                                        %
+% ---------------------------------------------------------------------- %
+
+impression_table([]) :- !, nl.
+impression_table([pers_var(P,V)|T]) :-
+    nl, format('~w ~d ~w ~a',['Place ',V, ' :', P]),
+    impression_table(T).
+
+% ---------------------------------------------------------------------- %
+%                                                                        %
+%                            BASE DE DONNEES                             %
+%                                                                        %
+% ---------------------------------------------------------------------- %
 
 epoux(anne,michel).
 epoux(manon,jules).
---
+++ stats.pl
@@ -0,0 +1,34 @@
+%------------------------------------------------%
+%     version 1:
+%------------------------------------------------%
+Place  1  : anne
+Place  2  : hugo
+Place  3  : manon
+Place  4  : tom
+Place  5  : alice
+Place  6  : jules
+Place  7  : juliette
+Place  8  : michel
+Place  9  : lucie
+Place  10  : jean
+Place  11  : charlotte
+Place  12  : louis
+Place  13  : olivia
+Place  14  : paul
+Place  15  : margaux
+Place  16  : antoine
+% Started at Tue Dec 12 22:47:48 2017
+% 3.640 seconds cpu time for 1,840,461 inferences
+% 10,278 atoms, 4,799 functors, 3,472 predicates, 67 modules, 204,389 VM-codes
+%
+%                        Limit    Allocated       In use
+% Local  stack:    128,004,096      258,048       78,480 Bytes
+% Global stack:    128,004,096      258,032      132,880 Bytes
+% Trail  stack:    268,435,456      129,016       15,816 Bytes
+%
+% 20 garbage collections gained 3,416,984 bytes in 0.014 seconds.
+% 5 clause garbage collections gained 193 clauses in 0.000 seconds.
+% Stack shifts: 9 local, 10 global, 6 trail in 0.001 seconds
+% 2 threads, 45 finished threads used 57.674 seconds
+true
+%--------------------------------------------------------
